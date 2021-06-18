7889fabab41b8789babd4eb47d9ec8d1b4e633ce
lbalbrecht <lalbrecht18@wooster.edu>
2021-06-18 07:36:47 -0700

    pushing for meeting

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -18,31 +18,31 @@ class Body extends Component {
     };
 
     handleInputChange = event => {
+        event.preventDefault()
         console.log('click')
-        // event.preventDefault()
         // take employees from state and search it for target value and set results
         this.setState({ search: event.target.value })
         console.log(this.state.search)
-        // this.state.employees.results.name.first.filter(event.target.value)
-        // console.log(event.target.value)
-        // .then(this.setState({ results: event.target.value }))
-        // .catch(err => console.log(err))
-        // this.setState({ results: this.state.search })
+        this.state.employees.results.name.first.filter(event.target.value)
+        console.log(event.target.value)
+        .then(this.setState({ results: event.target.value }))
+        .catch(err => console.log(err))
+        this.setState({ results: this.state.search })
     };
 
-    handleFormSubmit = event => {
-        // sort results
-        // piece of state to designate ascending or descending
-        // take current results and sort them
-        event.preventDefault();
-        // .then(res => {
-        //     if (res.data.status === "error") {
-        //         throw new Error(res.data.message)
-        //     }
-        //     this.setState({ employees: res.target.value, error: "" })
-        // })
-        // .catch(err => this.setState({ error: err.message }))
-    }
+    // handleFormSubmit = event => {
+    //     // sort results
+    //     // piece of state to designate ascending or descending
+    //     // take current results and sort them
+    //     event.preventDefault();
+    //     // .then(res => {
+    //     //     if (res.data.status === "error") {
+    //     //         throw new Error(res.data.message)
+    //     //     }
+    //     //     this.setState({ employees: res.target.value, error: "" })
+    //     // })
+    //     // .catch(err => this.setState({ error: err.message }))
+    // }
     render() {
         return (
             <div>
